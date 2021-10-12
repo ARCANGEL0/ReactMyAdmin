fe3c622771b846777da95943a65adae394346809
arc <arc@ARCANGEL0.arc>
2021-10-11 21:05:51 -0300

    Inicializacao para comecar a dashboard apos login

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,53 @@
 
 import React, {useRef} from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
 
+import LoginCmp from './Login'
 import Frame from './Frame'
-import Input from './Input'
-import Text from './Text'
-import Button from './Button'
 
 import './App.css';
 
 
-function App() {
-  const [loginCheck, setLogin] = React.useState('primary')
-  const [loginCheckTxt, setLoginTxt] = React.useState('primary')
-  const [active, setActive] = React.useState(false)
-
-  const loginFrame = useRef();
-  const loginU = useRef();
-  const loginP = useRef();
-
-  function removeFrame() {
-
-    loginFrame.current.removeFrame();
-    loginU.current.resetInput();
-    loginP.current.resetInput();
-
-  }
-  function ErrorLogin() {
-    setLogin('error');
-    setLoginTxt('red');
-      setTimeout(() => setLogin('primary'), 2000);
-      setTimeout(() => setLoginTxt('#7efcf6'), 2000);
-
-  }
-
-  function Login() {
-    setLogin('success')
-    setLoginTxt('chartreuse');
-      setTimeout(() => setLogin('primary'), 2000);
-      setTimeout(() => setLoginTxt('#7efcf6'), 2000);
-      setTimeout(() => removeFrame(), 2000)
-  }
+const App = ()=> {
 
 
-
-  function handleSubmit(event, form) {
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const axios = require('axios')
+    const [isLogged, setLoginState] = React.useState(false)
   
-
-
-    axios.post(`http://localhost:800/login`,  {user : data.get("user"), pass : data.get("pass")})
-    .then(res => {
-      if (res.data.connected) {
-      Login()
-
-      }
-      else {
-      ErrorLogin()
-      }
-
-
-    })
-  
-  }
-
 
   return (
 
   <div className="content">
 
    {/* frame login */}
-    <Frame ref={loginFrame} actv={active} theme={loginCheck} className='loginDiv'>
-      <form onSubmit={handleSubmit}>
-        <div className="loginPage">
-           <Text theme={loginCheckTxt} tipo='h4' >Usuário</Text>
-           <Input tipo="text" ref={loginU} theme={loginCheck} className="user" id="user" name="user"></Input>
-           <br />
-           <Text theme={loginCheckTxt} tipo='h4' >Senha</Text>
-           <Input tipo="password" ref={loginP} theme={loginCheck} name="pass" className="pass" id="pass"></Input>
+   <Router forceRefresh={true}>
 
-         
+    <Route
+                  path="/"
+                  exact
+                  
 
-           <div className="loginBtnDiv">
-             <Button theme={loginCheck} tipoTexto='h4' className='loginBtn'>
+                 >
+                <LoginCmp />
+                     </Route>
+
+
+<Route
+                  path="/dashboard"
+                  exact
+                  render={() => (
+                    <>
+   
+   
+   <Frame theme="success">Logged in</Frame>
+
+                    </>
+                  )}
+                  >
+                      </Route>
               
-               <Text theme={loginCheckTxt} tipo='h4' >
-          
-                Login
-               </Text>
-                </Button>
-           </div>
-
-        </div>
-      </form>   
-    </Frame>
+    
+    
+    </Router>
   </div>
     );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,111 @@
+
+import React, {useRef} from 'react'
+import { createBrowserHistory } from 'history'
+
+import Frame from './Frame'
+import Input from './Input'
+import Text from './Text'
+import Button from './Button'
+
+import './App.css';
+const  LoginCmp = () => {
+
+
+
+
+  const [loginCheck, setLogin] = React.useState('primary')
+  const [loginCheckTxt, setLoginTxt] = React.useState('primary')
+  const [active, setActive] = React.useState(false)
+
+  const [isLogged, setLoginState] = React.useState(false)
+
+  const loginFrame = useRef();
+  const loginU = useRef();
+  const loginP = useRef();
+
+  const history = createBrowserHistory({forceRefresh:true});
+
+  function removeFrame() {
+
+    loginFrame.current.removeFrame();
+    loginU.current.resetInput();
+    loginP.current.resetInput();
+
+  }
+  function ErrorLogin() {
+    setLogin('error');
+    setLoginTxt('red');
+      setTimeout(() => setLogin('primary'), 2000);
+      setTimeout(() => setLoginTxt('#7efcf6'), 2000);
+
+  }
+
+  function Login() {
+    setLogin('success')
+    setLoginTxt('chartreuse');
+      setTimeout(() => setLogin('primary'), 2000);
+      setTimeout(() => setLoginTxt('#7efcf6'), 2000);
+      setTimeout(() => removeFrame(), 2000)
+      setTimeout(()=> setLoginState(true),3000);
+
+  }
+
+
+
+  function handleSubmit(event, form) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const axios = require('axios')
+  
+
+
+    axios.post(`http://localhost:800/login`,  {user : data.get("user"), pass : data.get("pass")})
+    .then(res => {
+      if (res.data.connected) {
+      Login()
+
+      }
+      else {
+      ErrorLogin()
+      }
+
+
+    })
+  
+  }
+  if(isLogged){
+   history.push('/dashboard')
+  }
+
+  return (
+<>
+  
+
+    <Frame ref={loginFrame} actv={active} theme={loginCheck} className='loginDiv'>
+      <form onSubmit={handleSubmit}>
+        <div className="loginPage">
+           <Text theme={loginCheckTxt} tipo='h4' >Usuário</Text>
+           <Input tipo="text" ref={loginU} theme={loginCheck} className="user" id="user" name="user"></Input>
+           <br />
+           <Text theme={loginCheckTxt} tipo='h4' >Senha</Text>
+           <Input tipo="password" ref={loginP} theme={loginCheck} name="pass" className="pass" id="pass"></Input>
+
+         
+
+           <div className="loginBtnDiv">
+             <Button theme={loginCheck} tipoTexto='h4' className='loginBtn'>
+              
+               <Text theme={loginCheckTxt} tipo='h4' >
+          
+                Login
+               </Text>
+                </Button>
+           </div>
+
+        </div>
+      </form>   
+    </Frame>
+    </>
+    );
+}
+export default LoginCmp;
