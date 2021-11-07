9807d6e5ac77bfd0b880cda35807852958f6dd6e
arc <arc@ARCANGEL0.arc>
2021-11-06 21:22:51 -0300

    Criacao de datatables para tabelas do bd

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,64 +1,206 @@
 import './App.css';
-import React, { useImperativeHandle, forwardRef } from 'react';
+import React, {
+  useImperativeHandle, forwardRef
+}
+from 'react';
+import {
+  MDBDataTableV5
+}
+from 'mdbreact';
 
-import { AnimatorGeneralProvider } from '@arwes/animation';
-import { BleepsProvider } from '@arwes/sounds';
+import {
+  AnimatorGeneralProvider
+}
+from '@arwes/animation';
+import {
+  BleepsProvider
+}
+from '@arwes/sounds';
 import * as Arwes from '@arwes/core';
 import './App.css';
-import { useParams } from 'react-router';
-const SOUND_ASSEMBLE_URL = 'https://playground.arwes.dev/assets/sounds/typing.mp3';
-
-const animatorGeneral = { duration: { enter: 1500, exit: 200 } };
-const audioSettings = { common: { volume: 1.25 } };
-const playersSettings = { assemble: { src: [SOUND_ASSEMBLE_URL], loop: true } };
-const bleepsSettings = { assemble: { player: 'assemble' } };
-
-
-const  Frame = forwardRef((props, ref)=> {
-
-
-  const [activate, setActive] = React.useState(true);
-  const [Frame, setFrame] = React.useState(Arwes.FrameBox)
-    const {data} = useParams();
- function removeFrame() {
-   
-   setActive(false)
- }
-
- function Hexagon() {
-   setFrame(Arwes.FrameHexagon)
- }
-
- function Pentagon () { 
-   setFrame(Arwes.FramePentagon)
- }
- function Lines () {
-    setFrame(Arwes.FrameLines)
- }
- function Corner () { 
-   setFrame(Arwes.FrameCorners)
- }
- function Underlines () { setFrame(Arwes.FrameUnderline)
- }
-
- useImperativeHandle(ref, () => {
-  return {
-    removeFrame: removeFrame,
-    Hexagon: Hexagon,
-    Pentagon: Pentagon,
-    Lines: Lines,
-    Corner: Corner,
-    Underlines: Underlines
-  };
-});
-  return (
-<ul>
-    {props.cont.map(function(db){
-                        return <li>{db}</li>;
-                      })}
-      
-    </ul>
-  );
+import {
+  useParams
 }
-)
-export default Frame;
+from 'react-router';
+const SOUND_ASSEMBLE_URL =
+  'https://playground.arwes.dev/assets/sounds/typing.mp3';
+
+const animatorGeneral = {
+  duration: {
+    enter: 1500,
+    exit: 200
+  }
+};
+const audioSettings = {
+  common: {
+    volume: 1.25
+  }
+};
+const playersSettings = {
+  assemble: {
+    src: [SOUND_ASSEMBLE_URL],
+    loop: true
+  }
+};
+const bleepsSettings = {
+  assemble: {
+    player: 'assemble'
+  }
+};
+
+
+const Frame = forwardRef((props, ref) => {
+
+
+      const [activate, setActive] = React.useState(true);
+      const [Frame, setFrame] = React.useState(Arwes.FrameBox)
+
+      const [datatables, setDatatables] = React.useState({
+        columns: [{
+          label: 'Tabela',
+          field: 'tabela',
+          width: 150,
+          attributes: {
+            'aria-controls': 'DataTable',
+            'aria-label': 'Tabela',
+          },
+        }, ],
+
+        rows: [{
+          tabela: 'Tiger Nixon',
+
+        }, {
+          tabela: 'Garrett Winters',
+
+        }, {
+          tabela: 'Ashton Cox',
+
+        }, {
+          tabela: 'Cedric Kelly',
+
+        }, {
+          tabela: 'Airi Satou',
+        }, {
+          tabela: 'Brielle Williamson',
+
+        }, {
+          tabela: 'Herrod Chandler',
+
+        }, {
+          tabela: 'Rhona Davidson',
+
+        }, {
+          tabela: 'Colleen Hurst',
+
+        }, {
+          tabela: 'Sonya Frost',
+
+        }, {
+          tabela: 'Jena Gaines',
+
+        }, {
+          tabela: 'Quinn Flynn',
+
+        }, {
+          tabela: 'Charde Marshall',
+
+        }, {
+          tabela: 'Haley Kennedy',
+
+        }, {
+          tabela: 'Tatyana Fitzpatrick',
+
+        }, {
+          tabela: 'Michael Silva',
+
+        }, {
+          tabela: 'Paul Byrd',
+
+        }, ],
+      })
+      const {
+        data
+      } = useParams();
+
+
+      function removeFrame() {
+
+        setActive(false)
+      }
+
+      function Hexagon() {
+        setFrame(Arwes.FrameHexagon)
+      }
+
+      function Pentagon() {
+        setFrame(Arwes.FramePentagon)
+      }
+
+      function Lines() {
+        setFrame(Arwes.FrameLines)
+      }
+
+      function Corner() {
+        setFrame(Arwes.FrameCorners)
+      }
+
+      function Underlines() {
+        setFrame(Arwes.FrameUnderline)
+      }
+
+      useImperativeHandle(ref, () => {
+        return {
+          removeFrame: removeFrame,
+          Hexagon: Hexagon,
+          Pentagon: Pentagon,
+          Lines: Lines,
+          Corner: Corner,
+          Underlines: Underlines
+        };
+      });
+      const [checkbox1, setCheckbox1] = React.useState('');
+
+      const showLogs2 = (e) => {
+        setCheckbox1(e);
+      };
+
+      return ( < >
+
+
+
+        < MDBDataTableV5 hover multipleCheckboxes checkbox headCheckboxID =
+        'id4'
+        bodyCheckboxID = 'checkboxes4'
+        getValueCheckBox = {
+          (e) => {
+            showLogs2(e);
+          }
+        }
+
+
+
+        data = {
+          datatables
+        }
+        paginate = {
+          false
+        }
+
+        info = {
+          false
+        }
+        paginate = {
+          false
+        }
+        fullPagination searchTop searchBottom = {
+          false
+        }
+        materialSearch barReverse / >
+
+
+        < />  );
+
+
+      });
+
+    export default Frame;
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,25 +1,33 @@
-
-import React, {useRef, useEffect} from 'react'
+import React, {
+  useRef, useEffect
+}
+from 'react'
 
 import Frame from './Frame'
-import Content from './Content' 
+import Content from './Content'
 import Text from './Text'
 import Button from './Button'
-import { CgLogOut } from 'react-icons/cg';
+import {
+  CgLogOut
+}
+from 'react-icons/cg';
 
 import './App.css';
-import { useHistory } from 'react-router'
+import {
+  useHistory
+}
+from 'react-router'
 
-  
-const  Dashboard = () => {
 
-  
+const Dashboard = () => {
+
+
   const [databases, setDatabases] = React.useState([])
   const [tables, setTables] = React.useState([])
   const [active, setActive] = React.useState(false)
 
 
- 
+
   const header = useRef();
   const dashboardFrame = useRef();
   const tablesFrame = useRef();
@@ -32,166 +40,196 @@ const  Dashboard = () => {
 
   function logOut() {
     localStorage.removeItem('logged')
-        history.push('/')
+    history.push('/')
 
 
   }
- 
+
   function removeFrame() {
 
     dashboardFrame.current.removeFrame();
 
 
   }
-  
-  
+
+
   useEffect(() => {
-     async function getData() {
-      const { data } = await axios.get('http://localhost:800/getDatabases')
-        setDatabases(data)
+    async function getData() {
+      const {
+        data
+      } = await axios.get('http://localhost:800/getDatabases')
+      setDatabases(data)
     }
-    
+
     getData()
   }, [])
 
   let axios = require('axios')
 
 
-  if(!localStorage.getItem('logged')){
-   history.push('/')
+  if (!localStorage.getItem('logged')) {
+    history.push('/')
   }
 
-  function dataItem(databaseitem) { 
-    var url = 'http://localhost:800/Database/'+databaseitem
+  function dataItem(databaseitem) {
+    var url = 'http://localhost:800/Database/' + databaseitem
 
 
 
-
-
-
-
-
-
-
-
-
-    
-      axios.get(url)
-      .then(function (response) {
+    axios.get(url)
+      .then(function(response) {
 
         var result = response.data.map(element => Object.values(element)).flat()
-        
+
         console.log(result)
-            // use val
-         setTables(result)
-        
+          // use val
+        setTables(result)
 
-          
+
+
         // I need this data here ^^
-    })
+      })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    .catch(function (error) {
-        console.log(error);
+    .catch(function(error) {
+      console.log(error);
     });
 
 
   }
 
   React.useEffect(() => {
-   
 
 
 
-    
-// precisa se usar um loop para cada database e inserir no frame do render
-    
+    // precisa se usar um loop para cada database e inserir no frame do render
+
     tablesFrame.current.Pentagon()
     navFrame.current.Lines()
     contentMain.current.Corner()
-  },[] );
-  
-
-
-  return (
-<>
-  
-
-    <Frame ref={dashboardFrame} actv={active} theme='primary' className='dashDiv'>
-        <div className="dashContent">
-     <div className="leftPanel">
-    <Frame ref={header} actv={active} theme='error' className='header'>
-      <>
-      <Text as='h2'>
-ArcSQL <br/>
-
-<button className="logoutBtn" onClick={logOut}><CgLogOut/></button>
-</Text>
-</>
-</Frame>
-   
-    <Frame ref={tablesFrame} actv={active} theme='primary' className='tablesFrame'>
+  }, []);
 
 
 
-    <br />
-
-{databases.map(data => (
-  <>
- <li >
- <Frame ref = {databaseItem} actv={active} theme='primary' className='databaseItem'>
-
-<Text as="h1" >  <button style={{background: 'none', color: 'cyan'}} className="databaseItem" onClick={dataItem.bind(this, data.Database)} key={data.Database}>{data.Database}</button></Text>
-</Frame>
-
-</li><br />
-</>
-
-))}
-     
+  return ( < >
 
 
+    < Frame ref = {
+      dashboardFrame
+    }
+    actv = {
+      active
+    }
+    theme = 'primary'
+    className = 'dashDiv' >
+    < div className = "dashContent" >
+    < div className = "leftPanel" >
+    < Frame ref = {
+      header
+    }
+    actv = {
+      active
+    }
+    theme = 'error'
+    className = 'header' >
+    < >
+    < Text as = 'h2' >
+    ArcSQL < br / >
 
-</Frame>
-</div>
-<div className="rightPanel">
-  <div className="nav">
-<Frame ref={navFrame} actv={active} theme='primary' className='navBar'>
-<Text as='h2'>
-Visualizar | Estrutura | Pesquisar | Inserir | SQL | Gatilhos & Eventos & Functions |</Text>
-</Frame>
-</div>
-<div className="mainContent">
-<Frame ref={contentMain} actv={active} theme='primary' className='frameContent'>
-<Text as='h2'>
+    < button className = "logoutBtn"
+    onClick = {
+      logOut
+    } > < CgLogOut / > < /button> < /Text > < /> < /Frame >
 
-<Content cont={tables}>  </Content>
-
-</Text>
-</Frame>
-
-</div>
-</div>  
+    < Frame ref = {
+      tablesFrame
+    }
+    actv = {
+      active
+    }
+    theme = 'primary'
+    className = 'tablesFrame' >
 
 
-</div>
+
+    < br / >
+
+    {
+      databases.map(data => ( < >
+        < li >
+        < Frame ref = {
+          databaseItem
+        }
+        actv = {
+          active
+        }
+        theme = 'primary'
+        className = 'databaseItem' >
+
+        < Text as = "h1" > < button style = {
+          {
+            background: 'none',
+            color: 'cyan'
+          }
+        }
+        className = "databaseItem"
+        onClick = {
+          dataItem.bind(this, data.Database)
+        }
+        key = {
+          data.Database
+        } > {
+          data.Database
+        } < /button></Text >
+        < /Frame>
+
+        < /li><br / >
+        < />
+
+      ))
+    }
 
 
-    </Frame>
-    </>
-    );
+
+    < /Frame> < /div > < div className = "rightPanel" >
+    < div className = "nav" >
+    < Frame ref = {
+      navFrame
+    }
+    actv = {
+      active
+    }
+    theme = 'primary'
+    className = 'navBar' >
+    < Text as = 'h2' >
+    Visualizar | Estrutura | Pesquisar | Inserir | SQL | Gatilhos & Eventos &
+    Functions | < /Text> < /Frame >
+    <
+    /div>  < Frame ref = {
+    contentMain
+  }
+  actv = {
+    active
+  }
+  theme = 'primary'
+  className = 'frameContent' >
+
+
+    < Content cont = {
+      tables
+    } > < /Content>
+
+  < /Frame > < /div >
+
+
+  < /div>
+
+
+  < /Frame>
+
+
+  < / >
+);
 }
 export default Dashboard;
